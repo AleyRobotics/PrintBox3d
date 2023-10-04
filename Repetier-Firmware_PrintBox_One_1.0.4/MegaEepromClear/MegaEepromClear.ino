71f8e170d10aa58661c51e5a7d548b275eb6bf75
Yuri <>
2023-10-04 11:09:03 +0300

    фвв firmwares

---
+++ Repetier-Firmware_PrintBox_One_1.0.4/MegaEepromClear/MegaEepromClear.ino
@@ -0,0 +1,15 @@
+#include <EEPROM.h>
+
+void setup()
+{
+  // write a 0 to all 512 bytes of the EEPROM
+  for (int i = 0; i < 4096; i++)
+    EEPROM.write(i, 0);
+    
+  // turn the LED on when we're done
+  digitalWrite(13, HIGH);
+}
+
+void loop()
+{
+}
